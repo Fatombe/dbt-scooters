9e9d24e2838d84d298d2e5c4a39744f05d7263a9
Anna Collins <belrev@yahoo.com>
2024-09-04 11:20:12 +0000

    Миссия 5.1

---
+++ models/trips_prep.sql
@@ -0,0 +1,19 @@
+select
+id,
+user_id,
+scooter_hw_id,
+started_at,
+finished_at,
+start_lat,
+start_lon,
+finish_lat,
+finish_lon,
+distance as distance_m,
+cast(price as decimal(20,2)) / 100 as price_rub,
+extract(epoch from (finished_at - started_at)) as duration_s,
+case 
+    when price = 0 then true
+    else false
+end as isfree,
+date(started_at) as "date"
+from {{ source("scooters_raw", "trips")}}
