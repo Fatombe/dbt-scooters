ea449bd5296f852140baaa33a49aa9d3f893b1d8
Anna Collins <belrev@yahoo.com>
2024-09-11 08:07:04 +0000

    Миссия 6.1

---
+++ models/trips_users.sql
@@ -0,0 +1,16 @@
+{{ config(full_refresh = false) }}
+
+
+select
+    tp.*,
+    us.sex,
+    extract(year from tp.started_at) - extract(year from us.birth_date) as age
+from {{ ref("trips_prep") }} as tp
+left join {{ source("scooters_raw", "users") }} as us on tp.user_id = us.id
+{% if is_incremental() %}
+    where tp.id > (select max(id) from {{ this }})
+    order by tp.id
+    limit 7500
+{% else %}
+    where tp.id <= 7500
+{% endif %}
